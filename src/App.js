4257996a3010f08ad26cf9eb3e0f8d151b998ab3
Otmane Aatik <aaotmane@gmail.com>
2022-10-17 15:05:27 -0400

    ready for the first workshop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     Hello Nucamp!
+      <Navbar dark color='primary' sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={NucampLogo} alt="nucamp logo" />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
     </div>
   );
 }
